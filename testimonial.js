20d5ebfb77e54615c7d1e0373f52778868078da7
sheriff-gaye <sheriffgaye5@gmail.com>
2024-08-12 04:07:34 +0000

    all

---
+++ testimonial.js
@@ -0,0 +1,25 @@
+const testimonials = [
+    {
+        quote: "Working with Harper was an absolute pleasure. Her attention to detail and expertise in web development brought my vision to life. Highly recommend her services!",
+        image: './images/avatar2.jpg',
+        name: 'John Anderson',
+        job: 'Software Engineer'
+    },
+    {
+        quote: "Harper's web development skills are exceptional. He built a responsive and visually stunning website for my business. His professionalism and dedication to quality are unmatched", 
+        image: './images/avatar1.jpg',
+        name: 'Sarah Thompson',
+        job: 'Finance'
+    },
+    {
+        quote: "I am extremely satisfied with the results of working with Emily. She created a user-friendly and dynamic website that perfectly showcases my brand. She is a true professional.", image: './images/avatar3.jpg',
+        name: 'Mark Ramirez',
+        job: 'Food Sciencetist'
+    },
+    {
+        quote: "Harper exceeded my expectations as a web developer. He not only delivered a beautifully designed website but also provided ongoing support and maintenance. A reliable and talented developer!", image: './images/avatar5.jpg',
+        name: 'Laura Chen',
+        job: 'Store Manager'
+    },
+
+]
